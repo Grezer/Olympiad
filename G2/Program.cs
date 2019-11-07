481b07ae22953a80b14058cacc8d9ede8548dddc
Name <rzaevnr@list.ru>
2019-11-07 19:22:07 +0300

    G2 is ready

---
+++ G2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace G2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myString = Console.ReadLine().Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            long subjectCount = Convert.ToInt32(myString[0]) - 1;
+            long totalPoints = Convert.ToInt32(myString[1]) - 1;
+            Console.WriteLine((factorial(totalPoints) / (factorial(totalPoints - subjectCount) * factorial(subjectCount))) % (Math.Pow(10, 9) + 7));
+        }
+        public static long factorial(long input)
+        {
+            long result = 1;
+            for (long i = input; i > 0; i--)
+                result *= i;
+            return result;
+        }
+    }
+}
